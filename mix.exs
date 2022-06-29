39f0a52a369b51d0403f09aa6bfea6466e9fab74
dvic <info@dvic.io>
2022-06-29 12:21:50 +0200

    Configure heex formatter

--- mix.exs
+++ mix.exs
@@ -33,8 +33,7 @@ defmodule LvDemo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.10"},
-      {:phoenix_html, "~> 3.0"},
+      {:phoenix, "~> 1.6.10"}, {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0", only: :test},
